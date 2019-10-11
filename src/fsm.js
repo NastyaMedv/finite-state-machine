8ec0275a3564705a19940a83546887ca9c9c25eb
NastyaMedv <33265018+NastyaMedv@users.noreply.github.com>
2019-10-12 01:04:14 +0300

    FSM states

--- src/fsm.js
+++ src/fsm.js
@@ -3,38 +3,79 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+      if (config) {
+        this.states = config.states;
+        this.now = config.initial;
+      } else throw new SyntaxError('Error');
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+      return this.now;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+
+    changeState(state) {
+      if (this.states[state])
+        this.now = state;
+        else throw new SyntaxError('Error');
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+      let state = this.states[this.now].transitions[event];
+      if (state)
+        this.now = state;
+        else throw new SyntaxError('Error');
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+      this.now = 'normal';
+    }
 
+    findState(transitions, event) {
+      for (let key in transitions) {
+        if (key == event)
+          return true;
+      }
+      return false;
+    }
     /**
      * Returns an array of states for which there are specified event transition rules.
      * Returns all states if argument is undefined.
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+      if (event) {
+        let answer = [];
+        for (let key in this.states) {
+          if (this.findState(this.states[key].transitions, event))
+            answer.push(key);
+          }
+        return answer;
+      } else {
+        let answer = [];
+        for (let key in this.states) {
+          answer.push(key);
+        }
+        return answer;
+      }
+    }
 
     /**
      * Goes back to previous state.
